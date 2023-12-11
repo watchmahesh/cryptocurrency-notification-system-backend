c22600346e3730fa7323540817d84f4f493c860e
mahesh karki <karki22mahesh@gmail.com>
2023-12-11 17:05:06 +0545

    manual scrap data disabled

--- src/controllers/crypto.controller.ts
+++ src/controllers/crypto.controller.ts
@@ -1,17 +1,11 @@
 import asyncWrapper from '@utils/asyncWrapper'
 import { returnResponse } from '@utils/returnResponse'
 import cryptoServices from '@services/crypto.service'
-import scrapServices from '@services/scrap.service'
 
 
 
 // Endpoint for fetching cryptocurrencies
 export const getcryptocurrencies = asyncWrapper(async (req, res) => {
     const returns = await cryptoServices.find(req)
-    returnResponse(res, returns)
-  })
-
-export const manualScrap = asyncWrapper(async (req, res) => {
-    const returns = await scrapServices.scrapAndSave()
     returnResponse(res, returns)
   })
--- src/routes/v1/crypto.routes.ts
+++ src/routes/v1/crypto.routes.ts
@@ -1,8 +1,7 @@
 import { Router } from 'express'
-import { getcryptocurrencies, manualScrap } from '@controllers/crypto.controller'
+import { getcryptocurrencies } from '@controllers/crypto.controller'
 
 const cryptoRouter = Router()
 cryptoRouter.get('/list', getcryptocurrencies)
-cryptoRouter.get('/manual-scrap', manualScrap)
 
 export default cryptoRouter
